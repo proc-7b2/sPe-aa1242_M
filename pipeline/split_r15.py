478aa75e03aed5786d0d4c7052f273419c6301f0
proc-7b2 <dodomartbusiness@gmail.com>
2026-02-14 19:18:36 +0500

    Update split_r15.py

--- pipeline/split_r15.py
+++ pipeline/split_r15.py
@@ -1,1 +1,13 @@
+def split_mesh(mesh, labels):
+    submeshes = {}
+
+    for name, face_ids in labels.items():
+        if not face_ids:
+            continue
+
+        sub = mesh.submesh([face_ids], append=True)
+        if len(sub) > 0:
+            submeshes[name] = sub[0]
+
+    return submeshes
 
